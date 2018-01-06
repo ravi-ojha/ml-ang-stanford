f9bd2f4e7136bdc9093e6c6ea297470b15f755d2
Ravi Ojha <raviojha2105@gmail.com>
2018-01-06 22:47:24 +0530

    Solve first two questions
     - plot data
     - compute cost function

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -4,16 +4,19 @@ function J = computeCost(X, y, theta)
 %   parameter for linear regression to fit the data points in X and y
 
 % Initialize some useful values
-m = length(y); % number of training examples
+    m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+% You need to return the following variables correctly
+    J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+    % To self me looking at this formula after a loooong time.
+    % Try to arrive at this from the formula stated here https://docs.google.com/document/d/1Kdqi0h6aFoSKPoZmSp54RFd8xeYotTo0_1Y1HmW5bC8/edit#heading=h.2s58ztj33wgj
+    % Also, about the confusion of whether to use X*theta or theta'*X, think about the way we can get two matrices to multiply
+    J = sum((X * theta - y) .^ 2)/(2*m); % From the formula that was taught in class, we would tend to do theta' * X, but matrix multiplication is not possible that way.
 
 
 
