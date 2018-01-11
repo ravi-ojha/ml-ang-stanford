864168f1ce360f7c4b374ee5b287eac577c22fb1
Ravi Ojha <raviojha2105@gmail.com>
2018-01-12 02:24:58 +0530

    ex2: cost function for loistic regression using sigmoid and log thing
    Summary: This was pretty tough. Those notes were very handy.
    I wouldn't have solved it without them. Now I know why I couldn't get past
    this excersice in last attempt at this course.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,10 +20,23 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% We know that h(x) is sigmoid(x) now
+
+% X * theta will return 100 x 1 matrix
+TX = X * theta;
+
+% About the y' thing, we can again use that logic of how to multiply two matrices, if it is even possible.
+% We saw y * log(sigmoid(TX)) is not possible because for matrix multiplication we need M x N and A x B, where N == A
+% Here y is 100 x 1 and log(sigmoid(TX)) is 100 x 1, so we took y'
+% It's a trick to know when to use transpose, we should consult the equation we got in the docs
+J = (-1/m)*((y' * log(sigmoid(TX))) + ((1 .- y)' * log(1 .- sigmoid(TX))));
 
 
-
-
+% (TX-y) is 100 x 1
+% grad has to be 3 x 1
+% X is 100 x 3
+% So it should be (3 x 100) * (100 x 1)
+grad = (1/m)*(X' * (sigmoid(TX) - y));
 
 
 
